492a88cd4ef3aeda14bea5d1c985af8f870b5c99
Nicole Branagan <nicolecarolinebranagan@gmail.com>
2016-05-14 21:19:07 -0400

    Breakout clone

--- aspectlegend/subgame.js
+++ aspectlegend/subgame.js
@@ -1,6 +1,10 @@
+"use strict";
+
 var Subgame = function() {
     this.player.parent = this;
     PlayMusic("spiral");
+    this.maxstages = SubgameStages.length - 1;
+    this.getStage(0);
 }
 
 Subgame.prototype = {
@@ -9,6 +13,23 @@ Subgame.prototype = {
     stage: 0,
     
     objects: [],
+    
+    getStage: function(index) {
+        this.stage = index;
+        this.objects = [];
+        this.tiles = 0;
+        var stage = SubgameStages[index];
+        for (var i = 0; i < stage.tilemap.length; i++) {
+            var check = stage.tilemap[i];
+            var x = i % 10;
+            var y = Math.floor(i / 10);
+            
+            if (check == 0)
+                continue;
+            this.objects.push(new SubgameTile(x * 16 + 4, y * 8 + 8, check - 1))
+            this.tiles++;
+        }
+    },
     
     update: function() {
         this.timer++;
@@ -23,6 +44,10 @@ Subgame.prototype = {
                 this.objects.splice(index, 1);
             }
         }, this);
+        
+        if (this.tiles == 0) {
+            // get next stage
+        }
     },
     
     draw: function(ctx) {
@@ -52,10 +77,18 @@ Subgame.prototype = {
         ctx.clearRect(0, gamecanvas.height-16, gamecanvas.width, 16);
         drawText(ctx, 0, 144-16, " AREA  STAGE   CRYS ");
         drawNumber(ctx, 2*8, 144-8, 7, 2);
-        drawNumber(ctx, 9*8, 144-8, this.stage, 1);
+        drawText(ctx, 7*8, 144-8, (this.stage+1).toString() + "/" + (this.maxstages+1).toString(), 1);
         drawNumber(ctx, 16*8, 144-8, Game.crystals, 2);
         // draw keys, crystals
         ctx.drawImage(gfx.objects, 12 * 16, 0, 16, 16, 13*8, 144-16, 16, 16);
+    },
+    
+    die: function() {
+        // TODO: Animation or sound or something
+        if (this.stage == 0)
+            runner = Game;
+        else
+            this.getStage(this.stage - 1);
     },
     
     player: {
@@ -72,7 +105,7 @@ Subgame.prototype = {
             
             if (Controls.Shoot) {
                 Controls.Shoot = false;
-                this.parent.objects.push(new SubgameProjectile(true, this.x, this.y));
+                this.parent.objects.push(new SubgameProjectile(this.parent, true, this.x, this.y));
             }
             
             if (this.x < 8)
@@ -87,7 +120,8 @@ Subgame.prototype = {
         }
     },
 }
-var SubgameProjectile = function(up, x, y) {
+var SubgameProjectile = function(parent, up, x, y) {
+    this.parent = parent;
     this.up = up;
     this.x = x;
     this.y = y;
@@ -105,11 +139,63 @@ SubgameProjectile.prototype = {
             this.y--;
         else
             this.y++;
-        if (this.y < 0 || this.y > 128)
+        if (this.y > 128)
             this.active = false;
+        else if (this.y < 0)
+            this.up = false;
+            
+        if (this.up) {
+            for (var i = 0; i < this.parent.objects.length; i++) {
+                var e = this.parent.objects[i];
+                if (e instanceof SubgameTile) {
+                    if (Math.abs(this.x - e.x) <= 8 && Math.abs(this.y - e.y) <= 4) {
+                        //this.active = false;
+                        this.up = !this.up;
+                        e.active = false;
+                        this.parent.tiles--;
+                        break;
+                    }
+                }
+            }
+        }
+        
+        if (this.y > 90 && !this.up) {
+            if (Math.abs(this.x - this.parent.player.x) < 4 && 
+                Math.abs(this.y - this.parent.player.y) < 4)
+                this.parent.die();
+        }
     },
     
     draw: function(ctx) {
         ctx.drawImage(gfx.objects, 152 + 8 * this.aspect, 0, 8, 8, this.x - 4, this.y - 4, 8, 8)
     }
 }
+
+var SubgameTile = function(x, y, color) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+}
+
+SubgameTile.prototype = {
+    update: function() {
+        
+    },
+    
+    draw: function(ctx) {
+        ctx.drawImage(gfx.objects, 12*16 + this.color*16, 2*16, 16, 8, this.x - 4, this.y - 8, 16, 8)
+    }
+}
+
+var SubgameStages = [
+    {
+        tilemap: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 1, 2, 1, 2, 1, 2, 1, 2, 0,
+            1, 2, 1, 2, 1, 2, 1, 2, 1, 2,
+            0, 1, 2, 1, 2, 1, 2, 1, 2, 0,
+            1, 2, 1, 2, 1, 2, 1, 2, 1, 2,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        ]
+    },
+]
