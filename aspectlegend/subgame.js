d8aebc35f9c93a9d5ecedc273e548af6583c905a
Nicole Branagan <nicolecarolinebranagan@gmail.com>
2016-05-21 21:35:06 -0400

    Area 7-3

--- aspectlegend/subgame.js
+++ aspectlegend/subgame.js
@@ -4,6 +4,7 @@ var Subgame = function() {
     this.player.parent = this;
     this.playMusic();
     this.maxstages = SubgameStages.length - 1;
+    this.objects = [];
     //this.getStage(0);
 }
 
@@ -12,7 +13,7 @@ Subgame.prototype = {
     
     stage: -1,
     
-    lives: 3,
+    lives: 5,
     
     objects: [],
     
@@ -22,17 +23,27 @@ Subgame.prototype = {
     
     deathCount: 0,
     
+    paused: false,
+    
     playMusic: function() {
         PlayMusic("spiral");
     },
     
     getStage: function(index) {
         this.stage = index;
-        this.objects = [];
         this.tiles = 0;
         var stage = SubgameStages[index];
         this.stagedata = stage;
         this.enemies = stage.enemies.slice(0);
+        var newobjects = []
+        for (var i = 0; i < this.objects; i++) {
+            var obj = this.objects[i];
+            if (obj instanceof SubgameProjectile)
+                newobjects.push(obj);
+            else
+                newobjects.push(new Explosion(obj.x, obj.y));
+        }
+        this.objects = newobjects;
         for (var i = 0; i < stage.tilemap.length; i++) {
             var check = stage.tilemap[i];
             var x = i % 10;
@@ -47,6 +58,14 @@ Subgame.prototype = {
     },
     
     update: function() {
+        if (this.paused) {
+            if (Controls.Enter) {
+                Controls.Enter = false;
+                PauseMusic();
+                this.paused = false;
+            }
+            return;
+        }
         this.timer++;
         if (this.stage == -1 && this.timer > 100)
             this.getStage(0);
@@ -58,8 +77,14 @@ Subgame.prototype = {
             this.deathCount--;
             if (this.deathCount == 0)
                 this.dieFunc();
-        } else
+        } else {
             this.player.update();
+            if (Controls.Enter) {
+                this.paused = true;
+                Controls.Enter = false;
+                PauseMusic();
+            }
+        }
             
         if (this.winCount > 0) {
             this.winCount--;
@@ -110,7 +135,10 @@ Subgame.prototype = {
     
     drawUI: function(ctx) {
         ctx.clearRect(0, gamecanvas.height-16, gamecanvas.width, 16);
-        drawText(ctx, 0, 144-16, " AREA  STAGE   LIFE ");
+        var layoutstring = " AREA  STAGE   LIFE ";
+        if (this.paused)
+            layoutstring = " AREA  PAUSE   LIFE ";
+        drawText(ctx, 0, 144-16, layoutstring);
         drawNumber(ctx, 2*8, 144-8, 7, 2);
         drawText(ctx, 8*8, 144-8, ((this.stage == -1 ? 1 : this.stage+1).toString()) + "/" + (this.maxstages+1).toString(), 1);
         drawNumber(ctx, 16*8, 144-8, this.lives, 2);
@@ -253,13 +281,15 @@ SubgameProjectile.prototype = {
 
 var SubgameTile = function(x, y, color) {
     this.x = x;
-    this.y = y;
+    this.ygoal = y;
+    this.y = 0;
     this.color = color;
 }
 
 SubgameTile.prototype = {
     update: function() {
-        
+        if (this.y != this.ygoal)
+            this.y++;
     },
     
     draw: function(ctx) {
@@ -274,8 +304,13 @@ var SubgameEnemy = function(parent, x, type, freq) {
     this.frequency = freq;
     this.timer = 0;
     
-    if (this.timer == 0)
+    if (this.type == 0)
         this.update = this.birdbrain;
+    else if (this.type == 1) {
+        this.update = this.orbbrain;
+        this.downtimer = 8;
+        this.left = (this.x > 80);
+    }
 }
 
 SubgameEnemy.prototype = {
@@ -299,6 +334,35 @@ SubgameEnemy.prototype = {
                 this.move(+1);
         }
         if (this.timer % 64 == 0) {
+            this.parent.objects.push(new SubgameProjectile(this.parent, false, this.x, this.y));
+        }
+    },
+    
+    orbbrain: function() {
+        this.timer++;
+        if (this.y > this.ymax) {
+            this.y++;
+            if (this.y > (144 - 8))
+                this.active = false;
+            return;
+        } else if (this.timer % 2 == 0) {
+            if (this.downtimer == 0) {
+                if (this.left)
+                    this.x = this.x - 1;
+                else
+                    this.x = this.x + 1;
+                if (this.x >= (144) || this.x < 8) {
+                    this.downtimer = 16;
+                }
+            } else {
+                this.downtimer--;
+                this.y = this.y + 1;
+                if (this.downtimer == 0) {
+                    this.left = !this.left;
+                }
+            }
+        }
+        if (this.timer % 128 == 0) {
             this.parent.objects.push(new SubgameProjectile(this.parent, false, this.x, this.y));
         }
     },
@@ -342,31 +406,31 @@ var SubgameStages = [
                 freq: 128,
             },
             {
-                time: 2400,
+                time: 1200,
                 type: 0,
                 x: 80,
                 freq: 128,
             },
             {
-                time: 3600,
+                time: 2400,
                 type: 0,
                 x: 80,
                 freq: 128,
             },
         ],
         
-        loop_point: 4000
+        loop_point: 2700
 
     },
     {
         tilemap: [
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
             0, 0, 0, 0, 1, 1, 0, 0, 0, 0,
-            0, 1, 2, 0, 1, 1, 0, 1, 2, 0,
-            0, 2, 0, 0, 0, 0, 0, 0, 1, 0,
-            0, 1, 2, 1, 0, 0, 2, 1, 2, 0,
-            0, 0, 0, 2, 0, 0, 1, 0, 0, 0,
-            0, 1, 0, 1, 0, 0, 2, 0, 2, 0,
+            0, 2, 2, 0, 1, 1, 0, 2, 2, 0,
+            0, 2, 0, 0, 0, 0, 0, 0, 2, 0,
+            0, 2, 2, 2, 0, 0, 2, 2, 2, 0,
+            0, 0, 0, 2, 0, 0, 2, 0, 0, 0,
+            0, 1, 0, 2, 0, 0, 2, 0, 1, 0,
         ],
         
         enemies: [
@@ -410,5 +474,43 @@ var SubgameStages = [
         ],
         
         loop_point: 1500,
+    },
+    {
+        tilemap: [
+            0, 0, 0, 0, 2, 2, 0, 0, 0, 0,
+            0, 0, 2, 2, 1, 1, 2, 2, 0, 0,
+            2, 2, 1, 1, 2, 2, 1, 1, 2, 2,
+            1, 1, 2, 2, 1, 1, 2, 2, 1, 1,
+            2, 2, 1, 1, 2, 2, 1, 1, 2, 2,
+            1, 1, 2, 2, 0, 0, 2, 2, 1, 1,
+            2, 2, 0, 0, 0, 0, 0, 0, 2, 2,
+        ],
+        enemies: [
+            {
+                time: 900,
+                type: 1,
+                x: 80,
+                freq: 128,
+            },
+            {
+                time: 1000,
+                type: 1,
+                x: 80,
+                freq: 128,
+            },
+            {
+                time: 1100,
+                type: 1,
+                x: 80,
+                freq: 128,
+            },
+        ],
+        loop_point: 1101,
+    },
+    
+    {   
+        tilemap: [],
+        enemies: [],
+        loop_point: 100,
     },
 ]
